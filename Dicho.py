9c0ad70463602d60eeeaad8a2db4a926a26acbb8
Adel Kandi <kandiadel58@gmail.com>
2022-06-27 10:55:15 +0100

    New Update

--- Dicho.py
+++ Dicho.py
@@ -1,6 +1,7 @@
 import math
 from numpy import real 
-import numpy
+import numpy 
+
 
 # The function we wanna calculate there racins "Solutions"
 def f(x):
@@ -10,8 +11,8 @@ def f(x):
 # Dichotonomie Mthode of calculation :
 
 # The interval 
-a = int(input("Give us the first number of the interval "))
-b = int(input("Give us the second nimber of the interval "))
+a = int(input("Give us the first number of the interval: "))
+b = int(input("Give us the second nimber of the interval: "))
 
 #eps = real(input("Give us the epsilon number :")) # nombre of iteration 
 eps = 10**-2
@@ -19,21 +20,29 @@ eps = 10**-2
 n=1
 # the 
 while abs(a-b)/2>eps:
-    x = (a+b)/2
+    root = None  #intialise the root "solution " to None 
+    mid = (a+b)/2
     #print(f'abs((a-b)/2)>eps was True, x is {x}!')
     # check 
+    if f(mid) == 0 or abs(f(mid)) < eps:  
+            root = mid                    # This to  find out if the mid is close to the solution 
+            break
     
-    if numpy.number(f(a))*numpy.number(f(b)) <= 0:
-         a = a
-         b = numpy.number(f(b))
+    if f(a)*f(mid) <= 0:
+        a = mid             # make the a mid to do another mid also until it came up with the solution 
+
     else:
-        a = numpy.number(f(a))
-        b = b
+        b = mid             #make the b mid to do another mid also until it came up with the solution
+
     n = n + 1
 
 
 # Print the result:
+if root is None:
+    print('Root not found')
+else:
+    print(f'The root, according to the dichotomy method, is at the point x = {root}')  # print the solution as Root = x    f(root)=~o 
 
-print('the solution of this function is ',str(x))
+#print('the solution of this function is ',)
 print(n)
 # graph of the function :
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MN_Dicho
 This code can find a solution for your function using Numerical methods "Dichotomy Methode"
+
+-**commit changes** 
